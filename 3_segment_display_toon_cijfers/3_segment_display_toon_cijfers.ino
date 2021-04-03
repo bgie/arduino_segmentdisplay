f7b295ad199fd913cb77bd341a66d05da4cca314
brecht.kuppens <brecht.kuppens@gmail.com>
2021-04-03 10:39:47 +0200

    constanten aangepast

--- 3_segment_display_toon_cijfers/3_segment_display_toon_cijfers.ino
+++ 3_segment_display_toon_cijfers/3_segment_display_toon_cijfers.ino
@@ -1,125 +1,125 @@
-#define PIN_SEGMENT_PUNT          2
-#define PIN_SEGMENT_BOVEN         3
-#define PIN_SEGMENT_RECHTSBOVEN   4
-#define PIN_SEGMENT_RECHTSONDER   5
-#define PIN_SEGMENT_ONDER         6
-#define PIN_SEGMENT_LINKSONDER    7
-#define PIN_SEGMENT_LINKSBOVEN    8
-#define PIN_SEGMENT_MIDDEN        9
+#define SEGMENT_PUNT          2
+#define SEGMENT_BOVEN         3
+#define SEGMENT_RECHTSBOVEN   4
+#define SEGMENT_RECHTSONDER   5
+#define SEGMENT_ONDER         6
+#define SEGMENT_LINKSONDER    7
+#define SEGMENT_LINKSBOVEN    8
+#define SEGMENT_MIDDEN        9
 
 void setup() {
-  pinMode(PIN_SEGMENT_PUNT, OUTPUT);
-  pinMode(PIN_SEGMENT_BOVEN, OUTPUT);
-  pinMode(PIN_SEGMENT_RECHTSBOVEN, OUTPUT);
-  pinMode(PIN_SEGMENT_RECHTSONDER, OUTPUT);
-  pinMode(PIN_SEGMENT_ONDER, OUTPUT);
-  pinMode(PIN_SEGMENT_LINKSONDER, OUTPUT);
-  pinMode(PIN_SEGMENT_LINKSBOVEN, OUTPUT);
-  pinMode(PIN_SEGMENT_MIDDEN, OUTPUT);
+  pinMode(SEGMENT_PUNT, OUTPUT);
+  pinMode(SEGMENT_BOVEN, OUTPUT);
+  pinMode(SEGMENT_RECHTSBOVEN, OUTPUT);
+  pinMode(SEGMENT_RECHTSONDER, OUTPUT);
+  pinMode(SEGMENT_ONDER, OUTPUT);
+  pinMode(SEGMENT_LINKSONDER, OUTPUT);
+  pinMode(SEGMENT_LINKSBOVEN, OUTPUT);
+  pinMode(SEGMENT_MIDDEN, OUTPUT);
 }
 
 void toonCijfer(int cijfer)
 {
   switch(cijfer) {
     case 0:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_MIDDEN, HIGH);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, LOW);
+      digitalWrite(SEGMENT_LINKSONDER, LOW);
+      digitalWrite(SEGMENT_LINKSBOVEN, LOW);
+      digitalWrite(SEGMENT_MIDDEN, HIGH);
       break;
     case 1:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_MIDDEN, HIGH);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, HIGH);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSBOVEN, HIGH);
+      digitalWrite(SEGMENT_MIDDEN, HIGH);
       break;
     case 2:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_ONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_MIDDEN, LOW);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, HIGH);
+      digitalWrite(SEGMENT_ONDER, LOW);
+      digitalWrite(SEGMENT_LINKSONDER, LOW);
+      digitalWrite(SEGMENT_LINKSBOVEN, HIGH);
+      digitalWrite(SEGMENT_MIDDEN, LOW);
       break;
     case 3:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_MIDDEN, LOW);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, LOW);
+      digitalWrite(SEGMENT_LINKSONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSBOVEN, HIGH);
+      digitalWrite(SEGMENT_MIDDEN, LOW);
       break;
     case 4:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_MIDDEN, LOW);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, HIGH);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSBOVEN, LOW);
+      digitalWrite(SEGMENT_MIDDEN, LOW);
       break;
     case 5:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_MIDDEN, LOW);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, HIGH);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, LOW);
+      digitalWrite(SEGMENT_LINKSONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSBOVEN, LOW);
+      digitalWrite(SEGMENT_MIDDEN, LOW);
       break;
     case 6:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_MIDDEN, LOW);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, HIGH);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, LOW);
+      digitalWrite(SEGMENT_LINKSONDER, LOW);
+      digitalWrite(SEGMENT_LINKSBOVEN, LOW);
+      digitalWrite(SEGMENT_MIDDEN, LOW);
       break;
     case 7:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, HIGH);
-      digitalWrite(PIN_SEGMENT_MIDDEN, HIGH);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSBOVEN, HIGH);
+      digitalWrite(SEGMENT_MIDDEN, HIGH);
       break;
     case 8:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_MIDDEN, LOW);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, LOW);
+      digitalWrite(SEGMENT_LINKSONDER, LOW);
+      digitalWrite(SEGMENT_LINKSBOVEN, LOW);
+      digitalWrite(SEGMENT_MIDDEN, LOW);
       break;
     case 9:
-      digitalWrite(PIN_SEGMENT_PUNT, HIGH);
-      digitalWrite(PIN_SEGMENT_BOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_RECHTSONDER, LOW);
-      digitalWrite(PIN_SEGMENT_ONDER, LOW);
-      digitalWrite(PIN_SEGMENT_LINKSONDER, HIGH);
-      digitalWrite(PIN_SEGMENT_LINKSBOVEN, LOW);
-      digitalWrite(PIN_SEGMENT_MIDDEN, LOW);
+      digitalWrite(SEGMENT_PUNT, HIGH);
+      digitalWrite(SEGMENT_BOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSBOVEN, LOW);
+      digitalWrite(SEGMENT_RECHTSONDER, LOW);
+      digitalWrite(SEGMENT_ONDER, LOW);
+      digitalWrite(SEGMENT_LINKSONDER, HIGH);
+      digitalWrite(SEGMENT_LINKSBOVEN, LOW);
+      digitalWrite(SEGMENT_MIDDEN, LOW);
       break;
   }
 }
